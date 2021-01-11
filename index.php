ec32fe143089bcd1bdca0b9971816e837550201e
murgoori <murgoori@pocheoltech.hs.kr>
2021-01-12 01:13:46 +0900

    json

--- index.php
+++ index.php
@@ -1,48 +1,36 @@
 <!doctype html>
-
-
-
 <html lang="en">
-
 <head>
-
   <meta charset="utf-8">
-
-
-
   <title>Start Coding</title>
-
   <meta name="description" content="Start Coding">
-
   <meta name="author" content="SitePoint">
-
-
-
   <link rel="stylesheet" href="common/css/main.css">
   <link rel="stylesheet" href="common/css/reset.css">
   <link rel="stylesheet" href="common/css/common.css">
-
-
 </head>
-
-
-
 <body>
-
-  <script src="js/scripts.js"></script>
-
 	<table>
 		<tr colspan = "2">
 			<td>
 				 2020년 작품집 안녕 *^^* 또 네스트zzzaaa
 			</td>
-
+		</tr>
+		<tr>
 			<td>
-				 <a href = "2020/index.php"> 이동 </a>
+				<a href = "2020/index.php"> 이동 </a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a href = "HTML_TEST/index.html"> 1st HTML </a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a href = "JS_TEST/index.html"> 1st JAVASCRIPT </a>
 			</td>
 		</tr>
 	</table>
-
 </body>
-
 </html>
